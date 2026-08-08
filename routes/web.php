b5d729797ed3a8f2472a3a584d2dae917a1bf78b
Rai <yafiraihan25@gmail.com>
2026-08-08 16:56:04 +0700

    fix: tambah route redirect halaman root ke login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\Admin\PemeliharaanController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
